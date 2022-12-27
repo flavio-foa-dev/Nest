bbe33adaf899dd8e11085e4c898e2f6113e92bf7
Flavio Andrade <foa12381@gmail.com>
2022-12-27 11:14:15 -0300

    users/update/dto interface

--- store/src/domain/Users./user.controller.ts
+++ store/src/domain/Users./user.controller.ts
@@ -1,21 +1,44 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { CreatedUserDTO } from "./dto/createdUser";
-import { UserRepository } from "./users.repository";
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { CreatedUserDTO } from './dto/createdUser';
+import { IUserEntity } from './interfaces/User.entity';
+import { UserRepository } from './users.repository';
+import { v4 as uuid } from 'uuid';
+import { GetUsersAllDTO } from './dto/GetUserAllDTO';
+import { UpadatedUserDTO } from './dto/UpadateUser';
 
 @Controller('/users')
 export class UserController {
-  constructor( private userRepository:UserRepository ){}
-
-
+  constructor(private userRepository: UserRepository) {}
 
   @Post()
-  async createUser(@Body() user:CreatedUserDTO): Promise<String>{
-   this.userRepository.save(user)
-   return "created"
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  async createUser(@Body() user: CreatedUserDTO): Promise<Object> {
+    const userEntity = new IUserEntity();
+    userEntity.id = uuid();
+    userEntity.name = user.name;
+    userEntity.email = user.email;
+    userEntity.password = user.password;
+    this.userRepository.save(userEntity);
+    return {
+      id: userEntity.id,
+      name: userEntity.name,
+      message: 'created',
+    };
   }
 
   @Get()
-  async getUserAll(){
-    return this.userRepository.getUsersAll()
+  async getUserAll() {
+    const userAll = await this.userRepository.getUsersAll();
+    const parseUsers = userAll.map((user) => {
+      return (user = new GetUsersAllDTO(user.id, user.name));
+    });
+    return parseUsers;
+  }
+
+  @Put('/:id')
+  async updateUser(@Param('id') id: string, @Body() user: UpadatedUserDTO) {
+    const userUpdate = await this.userRepository.updatedUser(id, user);
+
+    return { user: userUpdate, message: 'Updated user successfully' };
   }
 }
--- store/src/helpers/validations/ProductNameUnique.ts
+++ store/src/helpers/validations/ProductNameUnique.ts
@@ -1,28 +1,39 @@
-import { Injectable } from "@nestjs/common";
-import { registerDecorator, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
-import { ProductRepository } from "src/domain/product/productor.repository";
-
+import { Injectable } from '@nestjs/common';
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { ProductRepository } from 'src/domain/product/productor.repository';
 
 @Injectable()
 @ValidatorConstraint({ async: true })
-export class ProductNameUniqueValidator implements ValidatorConstraintInterface {
-
+export class ProductNameUniqueValidator
+  implements ValidatorConstraintInterface
+{
   constructor(private productRepository: ProductRepository) {}
 
-  async validate(valueName: any, validationArguments?: ValidationArguments): Promise<boolean> {
+  async validate(
+    valueName: any,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> {
     const productNameUnique = await this.productRepository.getByName(valueName);
-    return !productNameUnique
+    return !productNameUnique;
   }
 }
 
-export const IsProductNameUnique = (optionsValidetion: ValidationOptions)=> {
-  return (object: Object, propert: string)=> {
+export const IsProductNameUnique = (optionsValidetion: ValidationOptions) => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return (object: Object, propert: string) => {
     registerDecorator({
       target: object.constructor,
       propertyName: propert,
       options: optionsValidetion,
       constraints: [],
-      validator: ProductNameUniqueValidator
-    })
-  }
-}
+      validator: ProductNameUniqueValidator,
+    });
+  };
+};
